fa799d88a613204391865aaa2c611f19f21bd398
NymScript <niyimorera28@gmail.com>
2024-08-06 09:39:15 -0500

    Pequeña actividad con Switch

--- primerApp/src/App.java
+++ primerApp/src/App.java
@@ -4,42 +4,46 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Que tipo de bebida te gusta?: ");
-        String bebida = scanner.nextLine();
-        
-        // Switch -> Es más adecuado cuando estás comparando una única variable con diferentes valores posibles, 
-        // haciendo que el código sea más legible y organizado.
+        System.out.println("¿Qué tipo de bebida te gusta?");
+        System.out.println("");
+        System.out.println("1. Cafe");
+        System.out.println("2. Mate");
+        System.out.println("3. Chocolate");
+        System.out.println("4. Leche");
+        System.out.println("");
+        System.out.print("Me gusta: ");
 
-        switch (bebida) {
-            case "Cafe":
-                System.err.println();
+        //Guardara en opcion un tipo de dato int y el scanner solo recibira int como tipo de dato entrada
+        int opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                System.out.println("Delicioso, mezclalo con Java");
                 break;
-            case "Leche":
-                System.out.println();
+            case 2:
+                System.out.println("Nariz de argentino debes tener");  
+                break;      
+            case 3:
+                System.out.println("Confirmooo, delicioso");
                 break;
-            case "Maria Juana":
-                System.out.println();
-                break;
+            case 4:
+                System.out.println("Buena combinacion para un browni");     
+                break;   
             default:
-                System.out.println("Ahh que bien");
+                System.out.println("Nah, pon una opcion de 1 al 4");
                 break;
         }
 
-        // if-else: para condiciones complejas y variadas que involucran operadores lógicos (como &&, ||)
-        // y comparaciones de múltiples valores o expresiones.
+        // Crtl k + Crtl c -> Comentar varias lineas
 
-        if(bebida == "Café"){
-            System.out.println("Buena elección");
-        }else if(bebida == "Mate"){
-            System.out.println("Che boludo ¿era de argenina?");
-        }else if(bebida == "Chocolate"){
-            System.out.println("Concuerdo totalmente");
-        }else if(bebida == "Aguapanela"){
-            System.out.println("Mucho estilo");
-        } else {
-            System.out.println("Que bien!");
-        }
+        // if (opcion <= 4) {
+        //     System.out.println("Disfruta la bebida");
+        // }
+        
+        System.out.println("Disfrutalo!");
 
+
+        //Cierro el escaner para que deje de escuchar
         scanner.close();
     }
 }
