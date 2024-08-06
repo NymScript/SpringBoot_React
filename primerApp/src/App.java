e8e8e1fa268777d2126fe4217cf4a0ffdc610da3
NymScript <niyimorera28@gmail.com>
2024-08-06 09:13:11 -0500

    Entendiendo el ifElse y el Switch en Java

--- primerApp/src/App.java
+++ primerApp/src/App.java
@@ -7,8 +7,26 @@ public class App {
         System.out.print("Que tipo de bebida te gusta?: ");
         String bebida = scanner.nextLine();
         
-        //Que tipo de bebida te gusta
+        // Switch -> Es más adecuado cuando estás comparando una única variable con diferentes valores posibles, 
+        // haciendo que el código sea más legible y organizado.
 
+        switch (bebida) {
+            case "Cafe":
+                System.err.println();
+                break;
+            case "Leche":
+                System.out.println();
+                break;
+            case "Maria Juana":
+                System.out.println();
+                break;
+            default:
+                System.out.println("Ahh que bien");
+                break;
+        }
+
+        // if-else: para condiciones complejas y variadas que involucran operadores lógicos (como &&, ||)
+        // y comparaciones de múltiples valores o expresiones.
 
         if(bebida == "Café"){
             System.out.println("Buena elección");
@@ -21,7 +39,8 @@ public class App {
         } else {
             System.out.println("Que bien!");
         }
-        
+
+        scanner.close();
     }
 }
 
